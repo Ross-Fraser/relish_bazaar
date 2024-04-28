42d32d64e83a7390726f488cd37bd27fb34dea9f
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-04-28 17:11:21 +0100

    create: setup secret key

--- relishBazaar/settings.py
+++ relishBazaar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--org3a!w*it5jg4qbxn5&z$&#h%dhbyedw1cj#3$v6a5!k)&=p'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
