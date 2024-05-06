8e3a53ea7d001ac55f45982de1877727cc6e08be
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-05-06 20:31:37 +0100

    create: add a categories model

--- coffee/models.py
+++ coffee/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    main_category = models.CharField(max_length=100)
+    sub_category = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
