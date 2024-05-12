4544cd2f1b0e4ee33a6424d6a344ed055e72cb2b
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-05-12 19:23:29 +0100

    rework: remove unique from main category

--- roastery/models.py
+++ roastery/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_id = models.BigAutoField(primary_key=True)
-    main_category = models.CharField(max_length=100, unique=True)
+    main_category = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
     sub_category = models.CharField(max_length=100)
     
     def __str__(self):
-        return self.name
+        return self.main_category
