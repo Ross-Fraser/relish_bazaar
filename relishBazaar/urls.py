ca5be11c63c73a26560c3f4062cba41fb4121778
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-06-06 20:01:28 +0100

    create: add registration form

--- relishBazaar/urls.py
+++ relishBazaar/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("roastery.urls"), name="roastery-urls"),
+    path('accounts/', include('accounts.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
