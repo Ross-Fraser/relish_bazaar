d9758f86cfe4f3c89bac37766f637defd2cc7d52
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-06-14 08:42:07 +0100

    rework: remove import from env to fix heroku deployment

--- relishBazaar/settings.py
+++ relishBazaar/settings.py
@@ -16,7 +16,6 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-from env import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
