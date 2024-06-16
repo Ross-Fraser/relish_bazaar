ea0454fae9e90c07b8ce53010ed56abd19404898
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-06-16 15:53:41 +0100

    rework: secure email settings

--- relishBazaar/settings.py
+++ relishBazaar/settings.py
@@ -102,13 +102,13 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
-# email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = 'True'
-EMAIL_HOST_USER = 'ross.fraser25@gmail.com'
-EMAIL_HOST_PASSWORD = 'nbij ikqj lvqv ehas'
+# Environment variables for email settings
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 DATABASES = {
