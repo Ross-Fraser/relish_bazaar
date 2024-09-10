abcbe25a63733e5022e3f45306e453c1571185d4
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-09-10 20:20:01 +0100

    create: add product delete confirmation dialog

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,14 @@
-document.getElementById("crud").addEventListener("click", function() {
-    goBack();
-});
+// Declare the variable only once
+let deleteProductId = null;
 
-function goBack() {
-    window.location.href = "{% url 'manage' %}";
+// Correctly set deleteProductId to the provided productId
+function showDeleteConfirmation(productId) {
+    deleteProductId = productId;
+    document.getElementById('delete-confirmation').style.display = 'block';
+}
+
+// Function to handle the cancellation of the delete action
+function cancelDelete() {
+    deleteProductId = null;
+    document.getElementById('delete-confirmation').style.display = 'none';
 }
