3cdb4cd7b75353dae7c3ffcd6a621a247400401d
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-09-15 21:41:45 +0100

    create: add bag views and urls

---
+++ bag/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.bag_detail, name='bag_detail'),
+    path('add/<int:product_id>/', views.add_to_bag, name='add_to_bag'),
+    path('update/<int:item_id>/<int:quantity>/', views.update_bag_item, name='update_bag_item'),
+    path('remove/<int:item_id>/', views.remove_from_bag, name='remove_from_bag'),
+]
--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import BagItem
+from roastery.models import Product
 
-# Create your views here.
+@login_required
+def bag_detail(request):
+    bag_items = BagItem.objects.filter(user=request.user)
+    total_price = sum(item.get_total_price() for item in bag_items)
+    return render(request, 'bag/bag_detail.html', {'bag_items': bag_items, 'total_price': total_price})
+
+@login_required
+def add_to_bag(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    bag_item, created = BagItem.objects.get_or_create(user=request.user, product=product)
+    bag_item.quantity += 1
+    bag_item.save()
+    return redirect('bag:bag_detail')
+
+@login_required
+def update_bag_item(request, item_id, quantity):
+    bag_item = get_object_or_404(BagItem, id=item_id, user=request.user)
+    bag_item.quantity = quantity
+    bag_item.save()
+    return redirect('bag:bag_detail')
+
+@login_required
+def remove_from_bag(request, item_id):
+    bag_item = get_object_or_404(BagItem, id=item_id, user=request.user)
+    bag_item.delete()
+    return redirect('bag:bag_detail')
