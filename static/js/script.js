fd16ccb17bfef5c3dc5fde4e1296eac324976c6e
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-09-22 23:22:44 +0100

    final: add js for leaving crud and collectstatic

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
-    alert("JavaScript is loaded correctly!");
 });
 
 window.addEventListener('DOMContentLoaded', function() {
     // Check if the current URL matches the Manage Products page
 if (window.location.pathname === '/manage/') {
-    alert("Manage Products page detected!");
     window.addEventListener('beforeunload', function (e) {
         var confirmationMessage = "Are you sure you want to leave this page?";
-        e.preventDefault();
-        e.returnValue = confirmationMessage; directly
         return confirmationMessage;
     });
 }
